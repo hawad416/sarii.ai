0992d0a106218a6ba1e078e818dfc63f2cfe186d
Hawa Ali <hawad416@gmail.com>
2023-03-06 16:57:05 -0800

    implemented basic mvp for sarii.ai

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sarii.ai
 Sarii AI : ChatGPT for University Student Support 
+
+Utilizes OpenAI's Completion API, and fine tuning to curate 
+responses and data to UW. 
---
+++ pages/api/generate.js
@@ -0,0 +1,55 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const animal = req.body.animal || '';
+  if (animal.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "You need 90 credsss",
+      }
+    });
+    return;
+  }
+
+  try {
+    const FINE_TUNED_MODEL = "curie:ft-personal-2023-03-06-10-36-14"
+    const completion = await openai.createCompletion({
+      model: FINE_TUNED_MODEL,
+      prompt: generatePrompt(animal),
+      temperature: 0,
+      max_tokens: 50,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(animal) {
+  return animal;
+}
