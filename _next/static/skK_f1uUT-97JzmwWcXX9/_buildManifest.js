e80e5725abd098c32c128f57107bde3443e4ec1f
hawad416 <hawad416@gmail.com>
2023-03-13 11:34:32 +0000

    Deploying to gh-pages from @ hawad416/sarii.ai@80ce2c21746f5d011caf810a9eb5cec9b9bb076b 🚀

---
+++ _next/static/skK_f1uUT-97JzmwWcXX9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c04383cd4c21f081.css","static/chunks/pages/index-7924d146e750cb0b.js"],"/_error":["static/chunks/pages/_error-19e73c3be68140bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
