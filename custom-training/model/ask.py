f1694acb36d9c73e4ae53472ca24cd4badb39aa9
Hawa Ali Drammeh <hdram3@cs.washington.edu>
2023-03-16 06:11:44 -0700

    makign stuff

--- custom-training/model/ask.py
+++ custom-training/model/ask.py
@@ -2,17 +2,6 @@ from model_base import construct_index, ask_ai
 from IPython.display import display, Markdown
 import os
 
-from flask import Flask, jsonify
+os.environ["OPENAI_API_KEY"] = "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"
 
-app = Flask(__name__)
-
-@app.route('/home/data')
-def get_data():
-    # your Python code to retrieve data goes here
-    data = {'response': ask_ai()}
-    return jsonify(data)
-
-if __name__ == '__main__':
-    os.environ["OPENAI_API_KEY"] = "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"
-
-    app.run(port=8080)
+ask_ai()
--- custom-training/model/model_base.py
+++ custom-training/model/model_base.py
@@ -6,6 +6,7 @@ from IPython.display import Markdown, display
 
 os.environ["OPENAI_API_KEY"] = "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"
 
+
 def construct_index(directory_path):
     # set maximum input size
     max_input_size = 4096
@@ -17,7 +18,7 @@ def construct_index(directory_path):
     chunk_size_limit = 600 
 
     # define LLM
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
  
     documents = SimpleDirectoryReader(directory_path).load_data()
@@ -31,8 +32,12 @@ def construct_index(directory_path):
     return index
 
 def ask_ai():
+    i = 0
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    query = "How do I become a competitive applicant?"
-    response = index.query(query, response_mode="compact")
-    return response.response
+
+    while(i < 120):
+        query = input("welcome to UW's AI chatbot! what is your question?")
+        response = index.query(query, response_mode="compact")
+        print(response.response)
+        i+=1
   
