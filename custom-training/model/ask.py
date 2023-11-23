bb6afdf1680289dd08ceb78b6dcc7d756ca8140b
Hawa Ali Drammeh <hawad416@gmail.com>
2023-11-22 22:54:14 -0800

    Update ask.py

--- custom-training/model/ask.py
+++ custom-training/model/ask.py
@@ -2,6 +2,6 @@ from model_base import construct_index, ask_ai
 from IPython.display import display, Markdown
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "REMOVED MY AI KEY"
 
 ask_ai()
